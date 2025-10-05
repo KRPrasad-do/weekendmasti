3fe41c4f14d0cbc97c4a3d212919e85654fd6dfd
KRPrasad-do <rprasadkokkiligadd@gmail.com>
2025-10-05 14:14:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Serve a simple HTML page with JS to get location
+    return render_template_string("""
+        <h2>Location Logger</h2>
+        <p id="status">Detecting location...</p>
+        <script>
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                fetch('/log-location', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude,
+                        accuracy: pos.coords.accuracy
+                    })
+                }).then(() => {
+                    document.getElementById('status').innerText = "Location sent to server!";
+                });
+            }, function(err) {
+                document.getElementById('status').innerText = "Location access denied.";
+            });
+        </script>
+    """)
+
+@app.route('/log-location', methods=['POST'])
+def log_location():
+    data = request.get_json()
+    print(f"Received location: {data}")
+    return '', 204  # No content
+
+if __name__ == '__main__':
+    app.run(debug=True)
